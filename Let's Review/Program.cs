d27c2e6c54707efcc4c4d8fae6b1bc8434039d59
ASVECode <albena_193@hotmail.com>
2021-01-14 21:28:32 +0200

    .

---
+++ Let's Review/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Let_s_Review
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            while (n != 0)
+            {
+                string str = Console.ReadLine();
+                string evenPositions = "";
+                string oddPositions = "";
+
+                for (int i = 0; i < str.Length; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        evenPositions += str[i];
+                    }
+                    else
+                    {
+                        oddPositions += str[i];
+                    }
+                }
+                Console.WriteLine(evenPositions + " " + oddPositions);
+                n--;
+            }
+        }
+    }
+}
