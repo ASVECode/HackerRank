3f54cfc9db39f36f6be277c8bab59371fcb33c8c
ASVECode <albena_193@hotmail.com>
2021-01-15 17:43:20 +0200

    .

---
+++ Arrays/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            //int.Parse(Console.ReadLine());
+            int[] arr = new int[] { 1, 2, 3, 4, 5 };
+            for (int i = n - 1; i > -1; --i)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+        }
+    }
+}
