bba6003dae1126482592c2f3d1a8b7afe464ff0f
ASVECode <albena_193@hotmail.com>
2021-02-01 22:03:09 +0200

    .

---
+++ More Linked Lists/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace More_Linked_Lists
+{
+    class Node
+    {
+        public int data;
+        public Node next;
+        public Node(int d)
+        {
+            data = d;
+            next = null;
+        }
+
+    }
+    class Program
+    {
+		public static Node removeDuplicates(Node head)
+		{
+			//Write your code here
+			var curr = head;
+			while (curr != null && curr.next != null)
+			{
+				while (curr.next != null && curr.data == curr.next.data)
+				{
+					curr.next = curr.next.next;
+				}
+				curr = curr.next;
+			}
+			return head;
+		}
+
+		public static Node insert(Node head, int data)
+		{
+			Node p = new Node(data);
+
+
+			if (head == null)
+				head = p;
+			else if (head.next == null)
+				head.next = p;
+			else
+			{
+				Node start = head;
+				while (start.next != null)
+					start = start.next;
+				start.next = p;
+
+			}
+			return head;
+		}
+		public static void display(Node head)
+		{
+			Node start = head;
+			while (start != null)
+			{
+				Console.Write(start.data + " ");
+				start = start.next;
+			}
+		}
+		static void Main(String[] args)
+		{
+			Node head = null;
+			int T = Int32.Parse(Console.ReadLine());
+			while (T-- > 0)
+			{
+				int data = Int32.Parse(Console.ReadLine());
+				head = insert(head, data);
+			}
+			head = removeDuplicates(head);
+			display(head);
+		}
+	}
+}
