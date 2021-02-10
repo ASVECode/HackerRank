13611c47b6f663f869b057d168c79092a1d7deec
ASVECode <albena_193@hotmail.com>
2021-02-10 21:21:35 +0200

    .

---
+++ Recursion 2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursion_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+            Console.WriteLine(Fact(n));
+        }
+
+        private static long Fact(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+            else
+            {
+                return n* Fact(n -1);
+            }
+        }
+    }
+}
