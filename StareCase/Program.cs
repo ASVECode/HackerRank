fc59ef1a1390f6f5ba7d17ca54d949f91ce04b60
ASVECode <albena_193@hotmail.com>
2021-02-12 21:03:51 +0200

    .

---
+++ StareCase/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StairCase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            staircase(N);
+        }
+        static void staircase(int n)
+        {
+            for (int i = 0; i < n; i++)
+                Console.WriteLine(new String('#', i + 1).PadLeft(n, ' '));
+        }
+
+    }
+}
