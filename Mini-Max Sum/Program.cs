d55f3671dde560bcb1554e21811f90a2d279fff4
ASVECode <albena_193@hotmail.com>
2021-02-13 20:59:06 +0200

    .

---
+++ Mini-Max Sum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mini_Max_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+         ;
+            miniMaxSum(arr);
+        }
+
+        static void miniMaxSum(int[] arr)
+        {
+            Array.Sort(arr);
+            long min = 0, max = 0;
+            for (int i = 0, j = arr.Length - 1; i < arr.Length - 1; i++, j--)
+            {
+                max = max + arr[j];
+                min = min + arr[i];
+            }
+            Console.WriteLine(min + " " + max);
+         
+        }
+    }
+}
