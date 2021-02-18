2950770f4b52d8144095ac64ec616c395af6a693
ASVECode <albena_193@hotmail.com>
2021-02-18 22:32:37 +0200

    .

---
+++ A Very Big Sum/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+namespace A_Very_Big_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int arCount = Convert.ToInt32(Console.ReadLine());
+
+            long[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt64(arTemp))
+            ;
+            long result = aVeryBigSum(ar);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+
+        private static long aVeryBigSum(long[] ar)
+        {
+            long sum = 0;
+            foreach (long a in ar)
+            {
+                sum += a;
+            }
+            return sum;
+        }
+    }
+}
+
