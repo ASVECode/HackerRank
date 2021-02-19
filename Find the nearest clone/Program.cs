fd1f8db5649fe1047968fc576d3e68ab8ed52882
ASVECode <albena_193@hotmail.com>
2021-02-19 23:04:53 +0200

    .

--- Find the nearest clone/Program.cs
+++ Find the nearest clone/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
 
 namespace Find_the_nearest_clone
 {
@@ -10,6 +7,44 @@ namespace Find_the_nearest_clone
     {
         static void Main(string[] args)
         {
+            // TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string[] graphNodesEdges = Console.ReadLine().Split(' ');
+            int graphNodes = Convert.ToInt32(graphNodesEdges[0]);
+            int graphEdges = Convert.ToInt32(graphNodesEdges[1]);
+
+            int[] graphFrom = new int[graphEdges];
+            int[] graphTo = new int[graphEdges];
+
+            for (int i = 0; i < graphEdges; i++)
+            {
+                string[] graphFromTo = Console.ReadLine().Split(' ');
+                graphFrom[i] = Convert.ToInt32(graphFromTo[0]);
+                graphTo[i] = Convert.ToInt32(graphFromTo[1]);
+
+                Console.WriteLine("--" + graphFrom[i] + " " + graphTo[i]);
+            }
+
+            long[] ids = Array.ConvertAll(Console.ReadLine()
+                .Split(' '), idsTemp => Convert.ToInt64(idsTemp));
+
+            int val = Convert.ToInt32(Console.ReadLine());
+
+            int ans = findShortest(graphNodes, graphFrom, graphTo, ids, val);
+
+            //textWriter.WriteLine(ans);
+
+            //textWriter.Flush();
+            //textWriter.Close();
+        }
+
+        public static int findShortest(int graphNodes, int[] graphFrom, int[] graphTo, long[] ids, int val)
+        {
+            int result = 0;
+
+            return result;
         }
     }
+
+
 }
---
+++ Hash Tables- Ice Cream Parlor/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hash_Tables__Ice_Cream_Parlor
+{
+    class Program
+    {
+        static void whatFlavors(int[] cost, int money)
+        {
+            HashSet<int> values = new HashSet<int>();
+            for (int i = 0; i < cost.Length; i++)
+            {
+                if (values.Contains(money - cost[i]))
+                {
+                    Console.WriteLine(money - cost[i] + " " + cost[i]);
+                    return;
+                }
+                values.Add(cost[i]);
+            }
+
+            throw new Exception("No two sum solution");
+
+        }
+        static void Main(string[] args)
+        {
+            int t = Convert.ToInt32(Console.ReadLine());
+
+            for (int tItr = 0; tItr < t; tItr++)
+            {
+                int money = Convert.ToInt32(Console.ReadLine());
+
+                int n = Convert.ToInt32(Console.ReadLine());
+
+                int[] cost = Array.ConvertAll(Console.ReadLine().Split(' '), costTemp => Convert.ToInt32(costTemp))
+                ;
+                whatFlavors(cost, money);
+            }
+
+        }
+    }
+}
