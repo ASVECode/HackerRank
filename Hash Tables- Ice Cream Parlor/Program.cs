efc5364789d160dc782fa6047b55b2148fde8286
ASVECode <albena_193@hotmail.com>
2021-02-20 19:40:27 +0200

    .

--- Hash Tables- Ice Cream Parlor/Program.cs
+++ Hash Tables- Ice Cream Parlor/Program.cs
@@ -7,15 +7,15 @@ namespace Hash_Tables__Ice_Cream_Parlor
     {
         static void whatFlavors(int[] cost, int money)
         {
-            HashSet<int> values = new HashSet<int>();
+            Dictionary<int, int> values = new Dictionary<int, int>();
             for (int i = 0; i < cost.Length; i++)
             {
-                if (values.Contains(money - cost[i]))
+                if (values.ContainsKey(money - cost[i]))
                 {
-                    Console.WriteLine(money - cost[i] + " " + cost[i]);
+                    Console.WriteLine(values[money - cost[i]]+1 + " " + (i+1));
                     return;
                 }
-                values.Add(cost[i]);
+                values.Add(cost[i], i);
             }
 
             throw new Exception("No two sum solution");
@@ -31,8 +31,9 @@ namespace Hash_Tables__Ice_Cream_Parlor
 
                 int n = Convert.ToInt32(Console.ReadLine());
 
-                int[] cost = Array.ConvertAll(Console.ReadLine().Split(' '), costTemp => Convert.ToInt32(costTemp))
-                ;
+                int[] cost = Array.ConvertAll(Console.ReadLine().Trim()
+                    .Split(' '), costTemp => Convert.ToInt32(costTemp));
+                
                 whatFlavors(cost, money);
             }
 
---
+++ Triple Sum/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace Triple_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string[] lenaLenbLenc = Console.ReadLine().Split(' ');
+
+            int lena = Convert.ToInt32(lenaLenbLenc[0]);
+
+            int lenb = Convert.ToInt32(lenaLenbLenc[1]);
+
+            int lenc = Convert.ToInt32(lenaLenbLenc[2]);
+
+            int[] arra = Array.ConvertAll(Console.ReadLine()
+                .Split(' '), arraTemp => Convert.ToInt32(arraTemp));
+
+            int[] arrb = Array.ConvertAll(Console.ReadLine()
+                .Split(' '), arrbTemp => Convert.ToInt32(arrbTemp));
+
+            int[] arrc = Array.ConvertAll(Console.ReadLine()
+                .Split(' '), arrcTemp => Convert.ToInt32(arrcTemp));
+            
+            long ans = triplets(arra, arrb, arrc);
+
+            textWriter.WriteLine(ans);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+
+        private static long triplets(int[] a, int[] b, int[] c)
+        {
+            a = a.Distinct().OrderBy(f => f).ToArray();
+            b = b.Distinct().OrderBy(f => f).ToArray();
+            c = c.Distinct().OrderBy(f => f).ToArray();
+
+            long i = 0;
+            long j = 0;
+            long sum = 0;
+
+            foreach (var val in b)
+            {
+                while (i < a.Length && a[i] <= val) i++;
+                while (j < c.Length && c[j] <= val) j++;
+
+                sum += i * j;
+            }
+
+            return sum;
+        }
+    }
+}
