9451c16e927f9d8d7106dcf13c98724790f01495
ASVECode <albena_193@hotmail.com>
2021-02-26 22:15:29 +0200

    .

---
+++ Array Manipulation/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+namespace Array_Manipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            string[] nm = Console.ReadLine().Split(' ');
+
+            int n = Convert.ToInt32(nm[0]);
+
+            int m = Convert.ToInt32(nm[1]);
+
+            int[][] queries = new int[m][];
+
+            for (int i = 0; i < m; i++)
+            {
+                queries[i] = Array.ConvertAll(Console.ReadLine().Split(' '), queriesTemp => Convert.ToInt32(queriesTemp));
+            }
+
+            long result = arrayManipulation(n, queries);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+
+        static long arrayManipulation(int n, int[][] queries)
+        {
+            long[] outputArray = new long[n + 2];
+            for (int i = 0; i < queries.Length; i++)
+            {
+                int a = queries[i][0];
+                int b = queries[i][1];
+                int k = queries[i][2];
+                outputArray[a] += k;
+                outputArray[b + 1] -= k;
+            }
+            long max = getMax(outputArray);
+            return max;
+        }
+
+        /**
+		 * @param inputArray
+		 * @return
+		 */
+        private static long getMax(long[] inputArray)
+        {
+            long max = Int32.MinValue;
+            long sum = 0;
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                sum += inputArray[i];
+                max = Math.Max(max, sum);
+            }
+            return max;
+        }
+    }
+}
