d798f929a9f63a90baf21a2f27d356e2b6e48432
ASVECode <albena_193@hotmail.com>
2021-02-28 21:11:35 +0200

    .

---
+++ JS solutions/Fraudulent Activity Notifications.js
@@ -0,0 +1,12 @@
+function activityNotifications(expenditure, d) {
+    let m;
+    for (let i = 0; i < expenditure.length; i++) {
+        if(d%2==0){
+
+        }
+
+    }
+
+
+}
+console.log(activityNotifications([10, 20, 30, 40, 50], 3));
