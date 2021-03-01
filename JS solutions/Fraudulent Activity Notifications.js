28a96858e913fd405b2e2a432aaee2e9c038dd3c
ASVECode <albena_193@hotmail.com>
2021-03-01 18:54:10 +0200

    .

--- JS solutions/Fraudulent Activity Notifications.js
+++ JS solutions/Fraudulent Activity Notifications.js
@@ -1,12 +1,15 @@
 function activityNotifications(expenditure, d) {
-    let m;
-    for (let i = 0; i < expenditure.length; i++) {
-        if(d%2==0){
-
-        }
-
+    let m, newArr = new Array(201).fill(0), notifications = 0;
+    for (let i = 0, j = d; i < expenditure.length; i++, j++) {
+        if (expenditure[j] == undefined) break;
+        newArr = expenditure.slice(i, j).sort();
+        m = d % 2 != 0 ? newArr[Math.floor(newArr.length / 2)] :
+            (newArr[Math.floor(newArr.length / 2 - 1)] + newArr[Math.floor(newArr.length / 2)]) / 2;
+        if (m * 2 <= expenditure[j]) notifications++;
     }
 
-
+    return notifications;
 }
-console.log(activityNotifications([10, 20, 30, 40, 50], 3));
+// console.log(activityNotifications([10, 20, 30, 40, 50], 3));
+// console.log(activityNotifications([1, 2, 3, 4, 4], 4));
+console.log(activityNotifications([2, 3, 4, 2, 3, 6, 8, 4, 5], 5));
